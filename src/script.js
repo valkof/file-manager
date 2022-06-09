c5c2978b9927db3adf82c6b30bfb110f6b38ecfb
valkof <vakofanov@g.bstu.by>
2022-06-09 20:59:56 +0300

    feat: exit from file manager

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import { stdin, stdout } from "process";
+
 const args = process.argv[2];
 
 function getUserName() {
@@ -8,7 +10,20 @@ function getUserName() {
   return args[11].toUpperCase() + args.slice(12);
 }
 
+function exitFileManager(userName) {
+  stdout.write(`Thank you for using File Manager, ${userName}!`);
+  process.exit(0);
+}
+
+async function startFileManager(userName) {
+  stdin.on('data', (data) => {
+    if (data.toString().trim() === '.exit') exitFileManager(userName);
+  });
+  process.on('SIGINT', () => exitFileManager(userName));
+}
+
 (async () => {
   const userName = getUserName();
   console.log(`Welcome to the File Manager, ${userName}`);
+  await startFileManager(userName);
 })()
